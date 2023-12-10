0f6ba6944c6a308a7b68cee2fe820ad21e575ff9
Xuyen <hoangxuyenpham30@gmail.com>
2023-12-10 17:40:24 +0700

    update

--- src/apis/axiosInstance.js
+++ src/apis/axiosInstance.js
@@ -3,7 +3,15 @@ import axios from "axios";
 export const axiosInstance = axios.create({
    baseURL: "http://localhost:3001/api/v1" || "https://travel-buddy-be.onrender.com/api/v1",
    timeout: 30000,
-   headers: {
-      "x-access-token": accessToken,
-   },
 });
+
+axiosInstance.interceptors.request.use((config) => {
+   const accessToken = JSON.parse(localStorage.getItem("accessToken"));
+
+   if (accessToken) {
+      config.headers["x-access-token"] = accessToken;
+   }
+
+   return config;
+});
+
